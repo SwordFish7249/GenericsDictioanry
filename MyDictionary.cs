d67355a9c9155bef53f94342c604d64623293b2c
CEM G <g.raing8@hotmail.com>
2021-01-21 22:31:24 +0200

    Dictionary - added GenericsDictionary

---
+++ MyDictionary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsDictionary
+{
+    class MyDictionary<Tkey,Tvalue>
+    {
+        Tkey[] tkeys;
+        Tvalue[] tvalues;
+        public MyDictionary()
+        {
+            tkeys = new Tkey[0];
+            tvalues = new Tvalue[0];
+        }
+        public void Add(Tkey tkey, Tvalue tvalue)
+        {
+            Tkey[] tempArray = tkeys;
+            tkeys = new Tkey[tkeys.Length + 1];
+            for(int i = 0; i<tempArray.Length; i++)
+            {
+                tkeys[i] = tempArray[i];
+
+                Tvalue[] _tempArray = tvalues;
+                tvalues = new Tvalue[tvalues.Length + 1];
+                for(int j = 0; j<_tempArray.Length; j++)
+                {
+                    tvalues[j] = _tempArray[j];
+                }
+                tvalues[tvalues.Length - 1] = tvalue;
+            }
+            tkeys[tkeys.Length - 1] = tkey;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericsDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> Liste = new MyDictionary<int, string>();
+            Liste.Add(1551551112, "ENGİN DEMİROĞ");
+            Liste.Add(1561561113, "BERKAY BİLGİN");
+            Liste.Add(1571571114, "EBRU SENA");
+            Liste.Add(1581581115, "ATEŞ");
+            Liste.Add(1591591116, "CANER GENÇDOĞAN");
+            Liste.Add(1511511117, "MÜCAHİT KAYA");
+            Liste.Add(0000251549, "CEM GÖKDEL");
+
+           
+        }
+    }
+}
